678771094c936023f5795a0cd691d9aff97f4868
Nahue941 <nahuelpaivag@gmail.com>
2021-03-25 13:08:58 -0300

    creados archivos preguntas, respuestas y test

---
+++ back/seed/answerSeed.js
@@ -0,0 +1,443 @@
+module.exports = [
+  /*----------test 1--------------*/
+  /*----------1-1--------------*/
+  {
+    questionId: 1,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 1,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 1,
+    answer: 'Respuesta 3, correcta',
+    correct: true
+  },
+  {
+    questionId: 1,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-2--------------*/
+  {
+    questionId: 2,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 2,
+    answer: 'Respuesta 2, correcta',
+    correct: true
+  },
+  {
+    questionId: 2,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 2,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-3--------------*/
+  {
+    questionId: 3,
+    answer: 'Respuesta 1, correcta',
+    correct: true
+  },
+  {
+    questionId: 3,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 3,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 3,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-4--------------*/
+  {
+    questionId: 4,
+    answer: 'Respuesta 1, correcta',
+    correct: true
+  },
+  {
+    questionId: 4,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 4,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 4,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-5--------------*/
+  {
+    questionId: 5,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 5,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 5,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 5,
+    answer: 'Respuesta 4, correcta',
+    correct: true
+  },
+  /*----------1-6--------------*/
+  {
+    questionId: 6,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 6,
+    answer: 'Respuesta 2, correcta',
+    correct: true
+  },
+  {
+    questionId: 6,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 6,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-7--------------*/
+  {
+    questionId: 7,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 7,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 7,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 7,
+    answer: 'Respuesta 4, correcta',
+    correct: true
+  },
+  /*----------1-8--------------*/
+  {
+    questionId: 8,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 8,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 8,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 8,
+    answer: 'Respuesta 4, correcta',
+    correct: true
+  },
+  /*----------1-9--------------*/
+  {
+    questionId: 9,
+    answer: 'Respuesta 1, correcta',
+    correct: true
+  },
+  {
+    questionId: 9,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 9,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 9,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-10--------------*/
+  {
+    questionId: 10,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 10,
+    answer: 'Respuesta 2, correcta',
+    correct: true
+  },
+  {
+    questionId: 10,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 10,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-11--------------*/
+  {
+    questionId: 11,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 11,
+    answer: 'Respuesta 2, correcta',
+    correct: true
+  },
+  {
+    questionId: 11,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 11,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-12--------------*/
+  {
+    questionId: 12,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 12,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 12,
+    answer: 'Respuesta 3, correcta',
+    correct: true
+  },
+  {
+    questionId: 12,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-13--------------*/
+  {
+    questionId: 13,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 13,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 13,
+    answer: 'Respuesta 3, correcta',
+    correct: true
+  },
+  {
+    questionId: 13,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------1-14--------------*/
+  {
+    questionId: 14,
+    answer: 'Respuesta 1, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 14,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 14,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 14,
+    answer: 'Respuesta 4, correcta',
+    correct: true
+  },
+  /*----------1-15--------------*/
+  {
+    questionId: 15,
+    answer: 'Respuesta 1, correcta',
+    correct: true
+  },
+  {
+    questionId: 15,
+    answer: 'Respuesta 2, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 15,
+    answer: 'Respuesta 3, incorrecta',
+    correct: false
+  },
+  {
+    questionId: 15,
+    answer: 'Respuesta 4, incorrecta',
+    correct: false
+  },
+  /*----------test 2--------------*/
+  /*----------2-1--------------*/
+  {
+    questionId: 16,
+    answer: 'Buenos Aires',
+    correct: true
+  },
+  {
+    questionId: 16,
+    answer: 'Montevideo',
+    correct: false
+  },
+  {
+    questionId: 16,
+    answer: 'Sangtiago De Chile',
+    correct: false
+  },
+  {
+    questionId: 16,
+    answer: 'Asunción',
+    correct: false
+  },
+  {
+    questionId: 16,
+    answer: 'La Paz',
+    correct: false
+  },
+  {
+    questionId: 16,
+    answer: 'Quito',
+    correct: false
+  },
+  /*----------2-2--------------*/
+  {
+    questionId: 17,
+    answer: 'Buenos Aires',
+    correct: false
+  },
+  {
+    questionId: 17,
+    answer: 'Montevideo',
+    correct: true
+  },
+  {
+    questionId: 17,
+    answer: 'Sangtiago De Chile',
+    correct: false
+  },
+  {
+    questionId: 17,
+    answer: 'Asunción',
+    correct: false
+  },
+  {
+    questionId: 17,
+    answer: 'La Paz',
+    correct: false
+  },
+  {
+    questionId: 17,
+    answer: 'Quito',
+    correct: false
+  },
+  /*----------2-3--------------*/
+  {
+    questionId: 18,
+    answer: 'Buenos Aires',
+    correct: false
+  },
+  {
+    questionId: 18,
+    answer: 'Montevideo',
+    correct: false
+  },
+  {
+    questionId: 18,
+    answer: 'Sangtiago De Chile',
+    correct: true
+  },
+  {
+    questionId: 18,
+    answer: 'Asunción',
+    correct: false
+  },
+  {
+    questionId: 18,
+    answer: 'La Paz',
+    correct: false
+  },
+  {
+    questionId: 18,
+    answer: 'Quito',
+    correct: false
+  },
+  /*----------2-4--------------*/
+  {
+    questionId: 19,
+    answer: 'Buenos Aires',
+    correct: false
+  },
+  {
+    questionId: 19,
+    answer: 'Montevideo',
+    correct: false
+  },
+  {
+    questionId: 19,
+    answer: 'Sangtiago De Chile',
+    correct: false
+  },
+  {
+    questionId: 19,
+    answer: 'Asunción',
+    correct: true
+  },
+  {
+    questionId: 19,
+    answer: 'La Paz',
+    correct: false
+  },
+  {
+    questionId: 19,
+    answer: 'Quito',
+    correct: false
+  },
+]
---
+++ back/seed/testSeed.js
@@ -0,0 +1,17 @@
+module.exports = [
+  {
+    name:'Test 1',
+    timeToComplete: 300,//seg
+    decription: 'Descripcion del test 1'
+  },
+  {
+    name:'Capitales-Paises',
+    timeToComplete: 300,//seg
+    decription: 'Descripcion del test 2'
+  },
+  {
+    name:'Test 3',
+    timeToComplete: 300,//seg
+    decription: 'Descripcion del test 3'
+  },
+]
