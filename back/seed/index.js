a6316e444d1597df1d80f27abc1f248762a6e7aa
Nahue941 <nahuelpaivag@gmail.com>
2021-03-25 17:25:34 -0300

    index de seed terminado

--- back/seed/index.js
+++ back/seed/index.js
@@ -1,5 +1,7 @@
-const { User, TestMade, Test, Question, Answer } = require("../models");
-const 
+const { Test, Question, Answer } = require('../models');
+const testArray = require('./testSeed');
+const questionArray = require('./answerSeed');
+const answerArray = require('./answerSeed');
 
 /** Al crear una tabla que contiene un FK. El método bulkCreate revisa que 
  *  dicho id exista en la tabla a la cual quiero relacionar. Por ende se debe
@@ -7,13 +9,35 @@ const
  *  tienen relación con ningunga otra tabla (en su interior no tienen FK), 
  *  y luego el resto.
  *  Orden de creación:
- *    1-Users
- *    2-Category
  * 
- *    3-Cart
- *    4-Product
+ *  1-Test
+ *  2-Questions
+ *  3-Answers
  * 
- *    5-Review
- *    6-Items
  *  *El orden no necesariamente debe ser este pero un orden posible.
  */
+
+
+
+ let testPromise = () => Test.bulkCreate(testArray)
+ .then(res => {
+   console.log(`-->Test creados`);
+   return res;
+ });
+
+ let questionPromise = () => Question.bulkCreate(questionArray)
+ .then(res => {
+   console.log(`-->Questions creadas`);
+   return res;
+ });
+
+ let answerPromise = () => Answer.bulkCreate(answerArray)
+ .then(res => {
+   console.log(`-->Answers creadas`);
+   return res;
+ });
+
+ return testPromise()
+  .then(() => questionPromise() )
+  .then(() => answerPromise() )
+  .then(() => console.log('----Seed terminado----'));
