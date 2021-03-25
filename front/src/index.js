254093e09683e5d10e282428cda9c5608b47b74d
Julieta Puig <jpuigperalta@gmail.com>
2021-03-25 16:41:28 -0300

    Listo config redux

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Main from "./Main"
+import {Provider} from "react-redux"
+import { BrowserRouter } from "react-router-dom";
+import store from "./state/store"
 
 ReactDOM.render(
+    <Provider store={store}>
   <React.StrictMode>
-    <h1>Hola ( ͡° ͜ʖ ͡°)</h1>
-    <img
-      src={'http://navent.com/es/wp-content/uploads/Navent-isologo.svg'}
-    ></img>
-    <Main />
-  </React.StrictMode>,
+    <BrowserRouter >
+    {/* <Main /> */}
+    <h1>Hola</h1>
+    </BrowserRouter>
+  </React.StrictMode>
+    </Provider>,
   document.getElementById('root'),
-);
+  );
+
+  {/* <img
+    src={'http://navent.com/es/wp-content/uploads/Navent-isologo.svg'}
+  ></img> */}
