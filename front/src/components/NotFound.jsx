5cb09f6e585daa2d33742476f5998c38801b4db1
agustindiazcano <agustin.td@gmail.com>
2021-04-01 15:47:57 -0300

    va queriendo

--- front/src/components/NotFound.jsx
+++ front/src/components/NotFound.jsx
@@ -4,20 +4,25 @@ import styles from '../styles/notFound.module.scss'
 const NotFound = () => {
   return (
     <div className={styles.container}>
-        <div>
+
+        <div className={styles.box1}>
             <img
             src={process.env.PUBLIC_URL + '/error-page-404.5d3d2368.svg'}
             alt="not found 404"
             />
         </div>
-    
-        <div className={styles.title}>
-            No encontramos la página que buscás
-        </div>
 
-        <div className={styles.subtitle}>
-            Cambió de identidad, se movió de lugar o dejó de existir.
-        </div>
+        <div className={styles.box2}>
+        
+            <div className={`${styles.title} ${styles.font}`}>
+                <div>No encontramos la</div> <div>página que buscás</div> 
+            </div>
+
+            <div className={`${styles.subtitle} ${styles.font}`}>
+                Cambió de identidad, se movió de lugar o dejó de existir.
+            </div>
+        
+        </div> 
 
     </div>
   );
