f37dd777c4f0a2400a073c250f976ac7f51665b1
Nahue941 <nahuelpaivag@gmail.com>
2021-04-05 15:53:02 -0300

    arreglo y ruteo del back

--- back/routes/User.js
+++ back/routes/User.js
@@ -1,3 +1,7 @@
-const express = require("express")
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const userController = require('../controllers/userController');
 
+router.post('/', userController.saveResult);
+
+module.exports = router;
