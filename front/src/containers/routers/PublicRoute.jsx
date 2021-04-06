aa52310bcc94defbd276df9272e28736d34b35f4
agustindiazcano <agustin.td@gmail.com>
2021-04-06 17:08:43 -0300

    estructura-rutas-front

---
+++ front/src/containers/routers/PublicRoute.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+
+const PublicRoute = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default PublicRoute
---
+++ front/src/containers/routers/Router.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Route } from 'react-router-dom';
+
+const Router = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Router
