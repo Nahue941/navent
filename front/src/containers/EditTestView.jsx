13f885d81c61fc176d090936617ca38008d57598
agustindiazcano <agustin.td@gmail.com>
2021-04-15 15:32:30 -0300

    cambios edit test front

--- front/src/containers/EditTestView.jsx
+++ front/src/containers/EditTestView.jsx
@@ -1,56 +1,85 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Button from '../components/UI/Button';
 import ButtonEdit from '../components/UI/ButtonEdit';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import ModalEdit from '../components/UI/ModalEdit';
-import { getEditTest } from '../state/test/actions';
-import Skill from '../components/Skill';
-const EditTestView = ({ skill, jijo }) => {
+import { getEditTest, setEditTest } from '../state/test/actions';
+const EditTestView = ({ skill, skillId }) => {
 
     const dispatch = useDispatch();
     const tests = useSelector((state) => state.test.editTest);
     const title = tests?.name
     const description = tests?.description
     const questions = tests?.questions// es un array de las preguntas, renderizo con un map
-    const answers = questions?.map((x,i, a) => x.answers[i]?.answer)
-    console.log(tests, "soy tests")
-    console.log(questions?.map(x=> x), "soy questions")
-    console.log(questions.map((x,i, a) => x.answers[i]?.answer), "soy answers")
+    const [showInput, setShowInput] = useState('true')
+    
+    
+    const changeState = (() => {
+        if(showInput=='true') setShowInput('false')
+        if(showInput=='false') setShowInput('true')
+    })
+    console.log(showInput)
+    
+    //Nueva data para el form
+    const newValues = {
+        title: title,
+        description: description,
+        questions: questions
+    }
+    console.log(newValues)
+    
     useEffect(() => {
-        dispatch(getEditTest(jijo));
+        dispatch(getEditTest(skillId));
     }, [dispatch]);
+
+
+const handleClick = (() => {
+    if(showInput=='true') setShowInput('false')
+        if(showInput=='false') setShowInput('true')
+})
+
+const handleInput = (() => {
+
+})
+
+//cuando hago click en guardar que haga un put de los nuevos valores al back
+
+const handleForm = (() => {
+    dispatch(getEditTest(newValues));
+})
 
     // {skills?.map((skill) => (
     //     <Skill key={skill.id} skill={skill} />
     //   ))}
+    // {showInput=='true'? <h3>show</h3>: <input placeholder="not show" key="1" ></input>}
     return (
         <div>
-
-
             <div>
-                <div><h1>Edit Test</h1></div>
-                <div><h3>{title}</h3>        <ButtonEdit color="blue" value="edit" /></div>
-                <div><h3>{description}</h3>        <ButtonEdit color="blue" value="edit" /></div>
-                <div><h3>{questions?.map(x => {
+                <div><h1>Editar Test</h1></div>
+                <div  onClick={() => changeState()}><ButtonEdit color="blue" value="editar" /></div>
+                <div > <label>Título: {showInput=='true'? <h3>{title}</h3>:<input placeholder={title} key={skillId}></input>} </label></div>
+                {showInput=='true'? <h3>{description}</h3>: <input placeholder={description} key="1" ></input>}
+                <div ><h3>{questions?.map(x => {
                     return (
                         <div key={x.id}>
-                            {x.question}
-                            <div>{x.answers?.map(answer => <div key={answer.id}>{answer.answer}</div>)}</div>
-                            <ButtonEdit color="blue" value="edit" />
+                            {<label>Pregunta: <input placeholder={x.question }></input></label>}
+                          <br/>  <label>Respuestas:<div>{x.answers?.map(answer => <div key={answer.id} >{showInput=='true'? <h3>{answer.answer}</h3>:<input placeholder={answer.answer} key={answer.id}></input>} </div>)}</div></label>
+                            
+                            
                         </div>)
                 })}</h3>
                 </div>
             </div>
-            <div>
+            <div onClick={() => handleForm()} >
                 <Link to="/admin/skill" >
-                    <Button color="blue" value="save" />
+                    <ButtonEdit color="blue" value="guardar" width="80px" />
                 </Link>
             </div>
 
             <div>
                 <Link to="/admin/skill">
-                    <Button color="blue" value="cancel" />
+                    <ButtonEdit color="blue" value="cancelar" width="80px"/>
                 </Link>
             </div>
 
--- front/src/containers/routers/AdminRoute.jsx
+++ front/src/containers/routers/AdminRoute.jsx
@@ -7,7 +7,7 @@ const AdminRoute = () => {
   return (
     <div>
       <Switch>
-        <Route path="/admin/skill/edit/:skillId" render={({match}) => <EditTestView jijo={match.params.skillId}>{match.params.skillId}</EditTestView>} />
+        <Route path="/admin/skill/edit/:skillId" render={({match}) => <EditTestView skillId={match.params.skillId}>{match.params.skillId}</EditTestView>} />
         <Route path="/admin/skill/create/:skillId" render={({match}) => <p>{match.params.skillId}</p> } />
         <Route path="/admin/skill" render={() => <AdminSkills />} />
         <Redirect exact from="/" to="admin/skill" />
