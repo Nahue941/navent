30d351d3691410e17c58b025e2d884335cda8696
Nahue941 <nahuelpaivag@gmail.com>
2021-04-15 12:00:42 -0300

    merge con el test edit de front

--- front/src/containers/EditTestView.jsx
+++ front/src/containers/EditTestView.jsx
@@ -7,16 +7,16 @@ import ModalEdit from '../components/UI/ModalEdit';
 import { getEditTest } from '../state/test/actions';
 import Skill from '../components/Skill';
 const EditTestView = ({ skill, jijo }) => {
-    
+
     const dispatch = useDispatch();
     const tests = useSelector((state) => state.test.editTest);
-    const title = tests.name
-    const description = tests.description
-    const questions = tests.questions// es un array de las preguntas, renderizo con un map
-    const answers = questions.map((x,i, a) => x.answers[i]?.answer)
-    console.log(tests, "soy tests")
-    console.log(questions?.map(x=> x), "soy questions")
-    console.log(questions.map((x,i, a) => x.answers[i]?.answer), "soy answers")
+    const title = tests?.name
+    const description = tests?.description
+    const questions = tests?.questions// es un array de las preguntas, renderizo con un map
+    const answers = questions?.map((x, i, a) => x.answers[i]?.answer)
+    /*  console.log(tests, "soy tests") */
+    /*  console.log(questions?.map(x=> x), "soy questions")
+     console.log(questions.map((x,i, a) => x.answers[i]?.answer), "soy answers") */
     useEffect(() => {
         dispatch(getEditTest(jijo));
     }, [dispatch]);
@@ -25,32 +25,37 @@ const EditTestView = ({ skill, jijo }) => {
     //     <Skill key={skill.id} skill={skill} />
     //   ))}
     return (
-    <div>
-
-
         <div>
-        <div><h1>Edit Test</h1></div>
-        <div><h3>{title}</h3>        <ButtonEdit  color="blue" value="edit" /></div>
-        <div><h3>{description}</h3>        <ButtonEdit  color="blue" value="edit" /></div>
-        <div><h3>{questions?.map(x=> <div key="1">{x.question}</div> )}</h3>
-        <div>{answers?.map(x=> <div key="2">{x}</div> )}</div>
-        <ButtonEdit color="blue" value="edit"/>
-        </div>
-        </div>
-        <div>
-        <Link to="/admin/skill" >
-        <Button color="blue" value="save" />
-        </Link>
-        </div>
 
-        <div>
-        <Link to="/admin/skill">
-        <Button color="blue" value="cancel" />
-        </Link>
-        </div>
 
-    </div>
-  );
+            <div>
+                <div><h1>Edit Test</h1></div>
+                <div><h3>{title}</h3>        <ButtonEdit color="blue" value="edit" /></div>
+                <div><h3>{description}</h3>        <ButtonEdit color="blue" value="edit" /></div>
+                <div><h3>{questions?.map(x => {
+                    return (
+                        <div key={x.id}>
+                            {x.question}
+                            <div>{x.answers?.map(answer => <div key={answer.id}>{answer.answer}</div>)}</div>
+                            <ButtonEdit color="blue" value="edit" />
+                        </div>)
+                })}</h3>
+                </div>
+            </div>
+            <div>
+                <Link to="/admin/skill" >
+                    <Button color="blue" value="save" />
+                </Link>
+            </div>
+
+            <div>
+                <Link to="/admin/skill">
+                    <Button color="blue" value="cancel" />
+                </Link>
+            </div>
+
+        </div>
+    );
 };
 
 export default EditTestView;
