9d54e2297d07fdeef4521b29db82481c7d561896
agustindiazcano <agustin.td@gmail.com>
2021-04-15 16:11:41 -0300

    tiempo del test y boolean respuestas agregado

--- front/src/containers/EditTestView.jsx
+++ front/src/containers/EditTestView.jsx
@@ -11,6 +11,9 @@ const EditTestView = ({ skill, skillId }) => {
     const tests = useSelector((state) => state.test.editTest);
     const title = tests?.name
     const description = tests?.description
+    const time = tests?.timeToComplete
+    // const answerBoolean = correct hacerlo con un checkbox
+    
     const questions = tests?.questions// es un array de las preguntas, renderizo con un map
     const [showInput, setShowInput] = useState('true')
     
@@ -59,12 +62,13 @@ const handleForm = (() => {
                 <div><h1>Editar Test</h1></div>
                 <div  onClick={() => changeState()}><ButtonEdit color="blue" value="editar" /></div>
                 <div > <label>Título: {showInput=='true'? <h3>{title}</h3>:<input placeholder={title} key={skillId}></input>} </label></div>
-                {showInput=='true'? <h3>{description}</h3>: <input placeholder={description} key="1" ></input>}
+                <label>Descripción: {showInput=='true'? <h3>{description}</h3>: <input placeholder={description} key="1" ></input>}</label>
+                <label>Tiempo: {showInput=='true'? <h3>{time}</h3>: <input placeholder={time} key="7" ></input>}</label>
                 <div ><h3>{questions?.map(x => {
                     return (
                         <div key={x.id}>
-                            {<label>Pregunta: <input placeholder={x.question }></input></label>}
-                          <br/>  <label>Respuestas:<div>{x.answers?.map(answer => <div key={answer.id} >{showInput=='true'? <h3>{answer.answer}</h3>:<input placeholder={answer.answer} key={answer.id}></input>} </div>)}</div></label>
+                            <label>Pregunta:{ <div><input placeholder={x.question }></input></div>}</label>
+                          <br/>  <label>Respuestas:<div>{x.answers?.map(answer => <div key={answer.id} >{showInput=='true'? <h3>{answer.answer} <span>{answer.correct.toString()}</span></h3>:<input placeholder={answer.answer} key={answer.id}></input>} </div>)}</div></label>
                             
                             
                         </div>)
